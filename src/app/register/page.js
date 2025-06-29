bcaa46b95c1c49ec314831df1dafaef2963f9c69
LandrieJ <landrie.jeanson@gmail.com>
2025-06-29 14:21:52 +0300

    'ooo'

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,5 @@
 // src/app/register/page.jsx
-import Registre from '@/components/Registre';
+import Registre from '../../components/Registre';
 
 export default function RegisterPage() {
   return <Registre language="fr" />;
