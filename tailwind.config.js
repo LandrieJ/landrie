34072eceff44643d9d5ab1504ff60a33a3a0b095
LandrieJ <landrie.jeanson@gmail.com>
2025-06-29 15:39:34 +0300

    '1539'

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Si ton code est dans src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
